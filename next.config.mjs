813f5b21ee4ff10df6a799b9eb0293799e04464f
Trovarelli <jtneto1995@hotmail.com>
2025-08-24 22:32:37 -0300

    fix: github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'portifolio';
+
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-}
-export default nextConfig
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: isProd ? `/${repo}` : undefined,
+  assetPrefix: isProd ? `/${repo}/` : undefined,
+};
+
+export default nextConfig;
